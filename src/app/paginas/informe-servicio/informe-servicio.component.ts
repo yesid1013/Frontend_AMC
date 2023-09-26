b793bc47a89e2923e5b23b19bb08359dfa790481
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-09-26 08:25:59 -0500

    Datatable en iforme de servicio

--- src/app/paginas/informe-servicio/informe-servicio.component.ts
+++ src/app/paginas/informe-servicio/informe-servicio.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { ADTSettings } from 'angular-datatables/src/models/settings';
+import { Subject } from 'rxjs';
 import { Servicio } from 'src/app/interfaces/servicio';
 import { ComunicationService } from 'src/app/servicios/comunication.service';
 import { ServicioService } from 'src/app/servicios/servicio/servicio.service';
@@ -12,10 +15,19 @@ export class InformeServicioComponent {
   isOpen = false;
   listaServicios: Servicio[] = [];
 
+  // DataTable
+  dtOptions: ADTSettings = {};
+  dtTrigger: Subject<any> = new Subject;
+  @ViewChild(DataTableDirective, { static: false })
+  dtElement!: DataTableDirective;
+
   constructor(private communicationService: ComunicationService, private servicio_service : ServicioService) {}
 
   ngOnInit() {
     this.obtener_servicios_sin_informe();
+    this.dtOptions = {
+      language: { url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json' }
+    };
     this.communicationService.sidebarOpen$.subscribe(isOpen => {
       this.isOpen = isOpen;
     });
@@ -23,7 +35,7 @@ export class InformeServicioComponent {
 
   obtener_servicios_sin_informe(){
     this.servicio_service.servicios_sin_informe().subscribe(data => {
-      this.listaServicios = data
+      this.listaServicios = data;
     })
   }
 
