7fde6f961fdc36b601a3a0f015f8ce1d239f63f7
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-10-18 08:44:52 -0500

    spinner y ocultar sidebar

--- src/app/paginas/informacion-permisos/informacion-permisos.component.ts
+++ src/app/paginas/informacion-permisos/informacion-permisos.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Permiso, Permisos_recibidos } from 'src/app/interfaces/permiso';
+import { ComunicationService } from 'src/app/servicios/comunication.service';
 import { PermisosService } from 'src/app/servicios/permisos/permisos.service';
 
 @Component({
@@ -13,6 +14,8 @@ export class InformacionPermisosComponent {
   public isTablaNovedadesColapsada: boolean = false;
   permisoId : string | null = null;
   permisoData: Permiso | null = null;
+  loading: boolean = false;
+  isOpen = false;
 
   ver_informacion_basica:number | undefined;
   ver_historial_servicios:number | undefined;
@@ -20,13 +23,16 @@ export class InformacionPermisosComponent {
   registrar_servicio:number | undefined;
   registrar_novedad:number | undefined;
 
-  constructor(private route: ActivatedRoute, private permisoService : PermisosService) {}
+  constructor(private route: ActivatedRoute, private permisoService : PermisosService, private communicationService: ComunicationService) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.permisoId = params.get('id_permiso');
     });
-    this.obtener_permiso()
+    this.obtener_permiso();
+    this.communicationService.sidebarOpen$.subscribe(isOpen => {
+      this.isOpen = isOpen;
+    });
   }
   
   public toggleTablaHistorial(): void {
@@ -38,7 +44,9 @@ export class InformacionPermisosComponent {
   }
 
   obtener_permiso(){
+    this.loading = true;
     this.permisoService.obtener_permiso(this.permisoId).subscribe(data => {
+      this.loading = false;
       this.permisoData = data;
       this.ver_informacion_basica = this.permisoData.ver_informacion_basica;   
       this.ver_historial_servicios = this.permisoData.ver_historial_servicios;
