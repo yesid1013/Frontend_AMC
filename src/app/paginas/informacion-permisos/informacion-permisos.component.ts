ee48d64a5b6cee88b1138283960dee674fbff575
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-10-19 09:34:06 -0500

    Registrar servicio

--- src/app/paginas/informacion-permisos/informacion-permisos.component.ts
+++ src/app/paginas/informacion-permisos/informacion-permisos.component.ts
@@ -5,6 +5,12 @@ import { Permiso, Permisos_recibidos } from 'src/app/interfaces/permiso';
 import { ActivoService } from 'src/app/servicios/activo/activo.service';
 import { ComunicationService } from 'src/app/servicios/comunication.service';
 import { PermisosService } from 'src/app/servicios/permisos/permisos.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { RegistroServicio } from 'src/app/interfaces/servicio';
+import { ServicioService } from 'src/app/servicios/servicio/servicio.service';
+
+
 
 @Component({
   selector: 'app-informacion-permisos',
@@ -19,15 +25,31 @@ export class InformacionPermisosComponent {
   activoData : Activo | null = null;
   loading: boolean = false;
   isOpen = false;
+  submitted = false;
 
   ver_informacion_basica:number | undefined;
   ver_historial_servicios:number | undefined;
   ver_novedades:number | undefined;
   registrar_servicio:number | undefined;
   registrar_novedad:number | undefined;
-  id_activo : string | undefined;
+  id_activo : string = '';
 
-  constructor(private route: ActivatedRoute, private permisoService : PermisosService, private communicationService: ComunicationService, private activoService : ActivoService) {}
+  selectedFile: File | null = null;
+  fileName: string | null = null;
+  fileMimeType: string | null = null;
+  fileContent: string | null = null;
+
+  // Formulario de registrar servicio
+  form_servicio: FormGroup = this.fb.group({
+    tipo_de_servicio: this.fb.control(null,[Validators.required]),
+    descripcion: this.fb.control(null, [Validators.required]),
+    observaciones: this.fb.control(null, []),
+    observaciones_usuario: this.fb.control(null, []),
+    fecha_ejecucion: this.fb.control('', [Validators.required]),
+    orden_de_servicio: this.fb.control('', [])
+  });
+
+  constructor(private route: ActivatedRoute, private permisoService : PermisosService, private communicationService: ComunicationService, private activoService : ActivoService,private fb: FormBuilder,private servicio_service: ServicioService) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -37,6 +59,28 @@ export class InformacionPermisosComponent {
     this.communicationService.sidebarOpen$.subscribe(isOpen => {
       this.isOpen = isOpen;
     });
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+
+    if (file) {
+      this.selectedFile = file;
+      this.fileName = file.name;
+      this.fileMimeType = file.type;
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.fileContent = e.target.result.split(',')[1];
+      };
+      reader.readAsDataURL(file);
+    } else {
+      // Si no se seleccionó un archivo, asignar valores nulos
+      this.selectedFile = null;
+      this.fileName = null;
+      this.fileMimeType = null;
+      this.fileContent = null;
+    }
   }
   
   public toggleTablaHistorial(): void {
@@ -70,6 +114,49 @@ export class InformacionPermisosComponent {
     this.activoService.info_activo(this.id_activo).subscribe(data => {
       this.activoData = data;
     })
+  }
+
+  crear_servicio(value : any){ 
+    this.submitted = true;    
+    if (this.form_servicio.valid){
+      const fecha = new Date(value.fecha_ejecucion);
+      const fechaFormateada = fecha.toISOString();
+
+      const servicio : RegistroServicio = {
+        fecha_ejecucion : fechaFormateada,
+        descripcion : value.descripcion,
+        id_tipo_servicio: value.tipo_de_servicio,
+        observaciones : value.observaciones,
+        observaciones_usuario : value.observaciones_usuario,
+
+        orden_de_servicio : {
+          name :this.fileName,
+          content : this.fileContent,
+          mimeType : this.fileMimeType
+        }
+      };
+
+      Swal.fire({
+        title: 'Cargando...',
+        allowOutsideClick: false,  // Impide que el usuario cierre el diálogo haciendo clic fuera
+        didOpen: () => {
+          Swal.showLoading();  // Muestra el spinner
+        }
+      });
+      
+      this.servicio_service.registrar_servicio(servicio,this.id_activo).subscribe(data => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Registro exitoso',
+          text: 'Servicio creado correctamente',
+          allowOutsideClick: false,
+        }).then((result) => {
+          if (result.isConfirmed) {
+
+          }
+        });
+      });
+    }
   }
   
 
