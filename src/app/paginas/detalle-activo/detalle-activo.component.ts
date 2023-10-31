c022c723cfaf57e18c10b30cf960d81e1da414c1
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-10-31 09:25:36 -0500

    Estructura de html en componente detalle_activo

--- src/app/paginas/detalle-activo/detalle-activo.component.ts
+++ src/app/paginas/detalle-activo/detalle-activo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ComunicationService } from 'src/app/servicios/comunication.service';
 
 
 @Component({
@@ -9,14 +10,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetalleActivoComponent {
   id_activo: string | null = null;
+  isOpen = false;
+
+  constructor(private route: ActivatedRoute,private communicationService: ComunicationService){}
 
 
   ngOnInit(){
     this.route.paramMap.subscribe(params => {
       this.id_activo = params.get('id_activo');
     });
+
+    this.communicationService.sidebarOpen$.subscribe(isOpen => {
+      this.isOpen = isOpen;
+    });
   }
 
-  constructor(private route: ActivatedRoute){}
+  
 
 }
