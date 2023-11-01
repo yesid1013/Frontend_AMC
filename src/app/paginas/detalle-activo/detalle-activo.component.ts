dd8d555107f03ef40a4e3164645338788157a666
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-11-01 10:02:58 -0500

    Adjuntar cotizacion

--- src/app/paginas/detalle-activo/detalle-activo.component.ts
+++ src/app/paginas/detalle-activo/detalle-activo.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Activo } from 'src/app/interfaces/activo';
-import { Adjuntar_informe_servicio, ServicioDeActivo } from 'src/app/interfaces/servicio';
+import { Adjuntar_cotizacion, Adjuntar_informe_servicio, ServicioDeActivo } from 'src/app/interfaces/servicio';
 import { ActivoService } from 'src/app/servicios/activo/activo.service';
 import { ComunicationService } from 'src/app/servicios/comunication.service';
 import { ServicioService } from 'src/app/servicios/servicio/servicio.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { CostoServicioService } from 'src/app/servicios/costo_servicio/costo-servicio.service';
 
 
 @Component({
@@ -27,10 +28,15 @@ export class DetalleActivoComponent {
   imageMimeType: string | null = null;
   imageContent: string | null = null;
 
-  constructor(private route: ActivatedRoute,private communicationService: ComunicationService,private activoService: ActivoService,private servicio_service: ServicioService,private fb: FormBuilder){}
+  constructor(private route: ActivatedRoute,private communicationService: ComunicationService,private activoService: ActivoService,private servicio_service: ServicioService,private fb: FormBuilder,private costo_servicio_service : CostoServicioService){}
 
   form_informe_servicio: FormGroup = this.fb.group({
     informe: this.fb.control(null, [Validators.required])
+  });
+
+  form_costo_servicio: FormGroup = this.fb.group({
+    costo: this.fb.control(null,[Validators.required]),
+    cotizacion: this.fb.control(null,[Validators.required])
   });
 
   ngOnInit(){
@@ -64,7 +70,7 @@ export class DetalleActivoComponent {
     this.form_informe_servicio.reset();
   }
 
-  onFileSelectedInforme(event: any) {
+  onFileSelected(event: any) {
     const file = event.target.files[0];
 
     if (file) {
@@ -131,6 +137,59 @@ export class DetalleActivoComponent {
             }
           }
           );
+        }
+      })
+
+    }
+  }
+
+  adjuntar_cotizacion(value : any){
+    this.submitted = true;
+    if (this.form_costo_servicio.valid){
+      Swal.fire({
+        title: '¿Estás seguro de adjuntar esta cotización?',
+        showDenyButton: true,
+        confirmButtonText: 'Adjuntar',
+        denyButtonText: `Cancelar`,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const costo_servicio: Adjuntar_cotizacion = {
+            costo : value.costo,
+            documento_cotizacion: {
+              name: this.imageName,
+              mimeType: this.imageMimeType,
+              content: this.imageContent
+            }
+          }
+
+          Swal.fire({
+            title: 'Cargando...',
+            allowOutsideClick: false,  // Impide que el usuario cierre el diálogo haciendo clic fuera
+            didOpen: () => {
+              Swal.showLoading();  // Muestra el spinner
+            }
+          });
+
+          this.costo_servicio_service.adjuntar_cotizacion(this.id_servicio, costo_servicio).subscribe({
+            next: (data) => {
+              Swal.close();
+              Swal.fire({
+                icon: 'success',
+                title: 'Servicio exitoso',
+                text: 'Cotización adjuntada correctamente',
+                allowOutsideClick: false,
+                footer: `<a href="${data.url_archivo}" target="_blank">Ver Cotización</a>`
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  this.obtener_servicios()
+                }
+                this.submitted = false;
+              });
+
+            }
+          }
+          );
+
         }
       })
 
