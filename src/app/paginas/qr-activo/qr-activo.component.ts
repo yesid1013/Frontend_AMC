b4b2d6f7885d4c8b7f5c7d651be19ecff64eca45
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-11-21 16:16:04 -0500

    Mostrar y ocultar contraseña

--- src/app/paginas/qr-activo/qr-activo.component.ts
+++ src/app/paginas/qr-activo/qr-activo.component.ts
@@ -8,6 +8,8 @@ import { UsuarioService } from 'src/app/servicios/usuario/usuario.service';
 import Swal from 'sweetalert2';
 import { HttpErrorResponse } from '@angular/common/http';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 
 
 
@@ -23,11 +25,32 @@ export class QrActivoComponent {
   activoData: InfoActivoQr | null = null;
   submitted = false;
   loading: boolean = false;
+  mostrarContrasena: boolean = false;
+
+  iconoMostrar: SafeHtml;
+  iconoOcultar: SafeHtml;
+
+
 
   //Modal
   modalRef?: BsModalRef;
 
-  constructor(private route: ActivatedRoute, private activoService : ActivoService,private fb: FormBuilder,private usarioService: UsuarioService, private router: Router,private modalService: BsModalService){
+  constructor(private route: ActivatedRoute, private activoService : ActivoService,private fb: FormBuilder,private usarioService: UsuarioService, private router: Router,private modalService: BsModalService, private sanitizer: DomSanitizer){
+    //Iconos para mostrar y ocultar constraseña
+    const svgMostrar = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+    <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+    </svg>`;
+    const svgOcultar = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" />
+    <path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" />
+    <path d="M3 3l18 18" />
+    </svg>`
+    
+    this.iconoMostrar = this.sanitizer.bypassSecurityTrustHtml(svgMostrar);
+    this.iconoOcultar = this.sanitizer.bypassSecurityTrustHtml(svgOcultar);
   }
 
   // Formulario de login
@@ -88,6 +111,11 @@ export class QrActivoComponent {
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
+
+  toggleMostrarContrasena() {
+    this.mostrarContrasena = !this.mostrarContrasena;
+}
+
 
 
     
