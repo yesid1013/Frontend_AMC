0d9d9edb7efa2a4eaae2a377a19567774454257d
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-11-21 10:01:52 -0500

    Formulario reactivo

--- src/app/paginas/qr-activo/qr-activo.component.ts
+++ src/app/paginas/qr-activo/qr-activo.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { InfoActivoQr } from 'src/app/interfaces/activo';
 import { ActivoService } from 'src/app/servicios/activo/activo.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-qr-activo',
@@ -11,10 +12,16 @@ import { ActivoService } from 'src/app/servicios/activo/activo.service';
 export class QrActivoComponent {
   id_activo: any = null;
   activoData: InfoActivoQr | null = null;
+  submitted = false;
 
-  constructor(private route: ActivatedRoute, private activoService : ActivoService){
-
+  constructor(private route: ActivatedRoute, private activoService : ActivoService,private fb: FormBuilder){
   }
+
+  // Formulario de login
+  form_login: FormGroup = this.fb.group({
+    correo: this.fb.control('', [Validators.required, Validators.email]),
+    contrasena: this.fb.control('', [Validators.required])
+  });
 
   ngOnInit(){
     this.route.paramMap.subscribe(params => {
@@ -27,7 +34,10 @@ export class QrActivoComponent {
     this.activoService.info_activo_qr(this.id_activo).subscribe(data =>{
       this.activoData = data;
     })
+  }
 
+  login(value: any){
+    this.submitted = true;
   }
 
 }
