20573d58dee45b1bd61daec98c3b80e9dcc2339e
Yesid Diaz <yesidiglesias1310@gmail.com>
2024-02-08 16:08:17 -0500

    Configuracion de google login en servicio

--- src/app/servicios/auth-google/auth-google.service.ts
+++ src/app/servicios/auth-google/auth-google.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGoogleService {
 
-  constructor() { }
+  constructor(private oAuthService: OAuthService) { }
+
+  initLogin(){
+    const config: AuthConfig = {
+      issuer : 'https://accounts.google.com',
+      strictDiscoveryDocumentValidation: false,
+      clientId : environment.clientId,
+      redirectUri: window.location.origin + '/main',
+      scope: 'openid profile email'
+    }
+
+    this.oAuthService.configure(config);
+    this.oAuthService.setupAutomaticSilentRefresh()
+    this.oAuthService.loadDiscoveryDocumentAndTryLogin();
+   }
 }
