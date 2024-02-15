2404a53f70fc6cb34e799e62fb72b24d62d232f5
Yesid Diaz <yesidiglesias1310@gmail.com>
2024-02-15 14:22:15 -0500

    Solicitud http al backend para validacion del login de google

--- src/app/servicios/auth-google/auth-google.service.ts
+++ src/app/servicios/auth-google/auth-google.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGoogleService {
 
-  constructor(private oAuthService: OAuthService) {
+  constructor(private oAuthService: OAuthService,private http: HttpClient) {
     this.initLogin()
    }
+   
+   url ='http://127.0.0.1:5000/api/v1/';
 
   initLogin(){
     const config: AuthConfig = {
@@ -28,6 +32,12 @@ export class AuthGoogleService {
    login(){
     this.oAuthService.initLoginFlow();
    }
+
+   login_google() : Observable<any>{
+    const tokenid = sessionStorage.getItem('id_token'); // obtengo el id token de google
+    return this.http.post(`${this.url}login_google`,{token_google : tokenid})
+  }
+
 
    logout(){
     this.oAuthService.logOut()
